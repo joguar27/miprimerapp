16324835d98d55cf2c353338c0c80b1362c2ecb3
jorge <jorgegusarruda@gmail.com>
2022-12-22 15:11:49 -0300

    agregue react-bs

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import {Button} from "react-bootstrap"
+
+const Main = () => {
+    return (
+        <main>
+            <h2>Home</h2>
+            {/*<button className="btn btn-primary">button</button>*/}
+            <Button>button de react-bootstrap</Button>
+        </main>
+    )
+}
+export default Main
