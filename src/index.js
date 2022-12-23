80c2b6d0b1b2e713ca9aa21da568a43e8d5012be
jorge <jorgegusarruda@gmail.com>
2022-12-23 00:37:56 -0300

    ult commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 //import bootstrap css
-import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap/dist/css/bootstrap.min.css'
 
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
