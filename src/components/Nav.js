843eda41d9362942ed1b84da206e195c6fe832b8
jorge <jorgegusarruda@gmail.com>
2022-12-26 12:45:20 -0300

    commit 2 lunes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,40 +1,40 @@
 const Nav = (props) => {
+  //const { isHeader : isHeader } = props
+  const { isHeader, textLinkFooter, hrefLinkFooter } = props;
 
-    
+  // const isHeader = props.isHeader || false
+  // const textLinkFooter = props.textLinkFooter || ""
+  // const hrefLinkFooter = props.hrefLinkFooter || ""
 
-    //const { isHeader : isHeader } = props
-    const { isHeader, textLinkFooter, hrefLinkFooter } = props
+  if (isHeader) {
+    return (
+      <nav className="header__navbar">
+        <a className="header__link" href="#">
+          INICIO
+        </a>
+        <a className="header__link" href="#">
+          PRODUCTOS
+        </a>
+        <a className="header__link" href="#">
+          CONTACTO
+        </a>
+        <a href="#">
+          <span className="counter-cart">100</span>
+          <span className="material-icons">shopping_cart</span>
+        </a>
+        {/* <a className="contador__carrito" href="#">0</a> */}
+      </nav>
+    );
+  } else {
+    return (
+      <nav className="header__navbar">
+        <a href={props.hrefLinkFooter}>{props.textLinkFooter}</a>
+      </nav>
+    );
+  }
+};
 
-    // const isHeader = props.isHeader || false
-    // const textLinkFooter = props.textLinkFooter || ""
-    // const hrefLinkFooter = props.hrefLinkFooter || ""
-
-    if (isHeader) {
-        return (
-            <nav className="header__navbar">
-                <a className="header__link" href="#">INICIO</a>
-                <a className="header__link" href="#">PRODUCTOS</a>
-                <a className="header__link" href="#">CONTACTO</a>
-                <a href="#">
-                <span className="material-icons">shopping_cart</span>
-                <span className="counter-cart">100</span>
-                </a>
-                {/* <a className="contador__carrito" href="#">0</a> */}
-            </nav>
-        )
-    } else {
-        return (
-            <nav className="header__navbar">
-                <a href={props.hrefLinkFooter}>{props.textLinkFooter}</a>
-            </nav>
-        )
-    }
-}
-
-export default Nav
-
-
-
+export default Nav;
 
 //(props) /linea 1
 //<span className="material-icons">shopping_cart</span> /linea 17
